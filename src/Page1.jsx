6dad36a97e2566f8b273ff88baa8913bcf4191db
Your Nameemail <shareef.99997@gmail.com>
2024-07-24 12:08:48 +0300

    set pages

---
+++ src/Page1.jsx
@@ -0,0 +1,14 @@
+function Page1() {
+
+  return (
+   <div>
+    <h1> Page 1 </h1>
+    <div className='Buttons'> 
+      <a href="/"><button>Home</button></a>
+      <a href="/test-vite/page2"><button>Page2</button></a>
+    </div>
+   </div>
+  )
+}
+
+export default Page1
---
+++ src/Page2.jsx
@@ -0,0 +1,14 @@
+function Page2() {
+
+  return (
+   <div>
+    <h1> Page 2 </h1>
+    <div className='Buttons'> 
+      <a href="/"><button>Home</button></a>
+      <a href="/test-vite/page1"><button>Page1</button></a>
+    </div>
+   </div>
+  )
+}
+
+export default Page2
